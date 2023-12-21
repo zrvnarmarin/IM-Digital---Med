d9f0b3414f91387b9d1ebb212a8b26397a37bd05
76marin76 <76marin76@gmail.com>
2023-12-21 11:00:53 +0100

    hero section redesigned; added full size image

--- app/(home-sections)/HeroSection.tsx
+++ app/(home-sections)/HeroSection.tsx
@@ -2,51 +2,31 @@ import Image from 'next/image';
 import React from 'react';
 import PlayButton from '../../public/play_button.webp';
 import Med from '../../public/med.png';
-import SquareBg from '../../public/square-background.png';
+// import HeroImage from '../../public/jc-gellidon-9Eb-bpTXglM-unsplash.jpg';
+import HeroImage from '../../public/heroDoctors.avif'; 
+
 
 export default function HeroSection() {
   return (
-    <section className='flex gap-8 lg:h-[51rem] flex-col lg:flex-row pb-12 pt-8 px-8 '>
-
-      <div className='flex flex-col justify-center lg:gap-12'>
-
-        <div>
-          <h1 className='font-bold text-start md:text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-[#2a2f31] leading-10'>
-            Became reasons of Thousand smiles
-          </h1>
-          <p className='text-[#555f60] text-start md:text-center text-md sm:text-lg leading-7 pt-6'>
-            We will help you in maintaining your health here we choose you a doctor who is experienced and certified in his field.
-          </p>
-        </div>
-
-        <div className='flex flex-col sm:flex-row items-center gap-4 lg:gap-12 pt-8'>
-          <div className='w-full'>
-            <button className='w-full text-white text-md lg:text-lg xl:text-xl font-semibold leading-7 bg-[#3caeaa] hover:bg-[#47ccc7] shadow-xl hover:scale-105 duration-100 px-6 py-3 lg:px-8 lg:py-4 rounded-md'>
-              Appointment
-            </button>
-          </div>
-          <div className='flex flex-row gap items-center justify-center w-full border-[#3caeaa] border rounded-md'>
-            <Image src={PlayButton} width={45} height={45} alt='Play_button' />
-            <p className='text-[#3f4749] text-md lg:text-lg xl:text-xl font-semibold leading-7 px-6 py-3 lg:px-8 lg:py-4 rounded-md'>Watch Video</p>
+    <section className='flex'>
+      <div className="min-h-screen w-full">
+        {/* Hero Section */}
+        <div className="relative h-screen md:h-auto bg-black">
+          <Image
+            src={HeroImage}
+            alt="Hero Image"
+            className='object-cover w-full h-full md:h-auto opacity-100 bg-black'
+          />
+          {/* Hero Content (Add your text and buttons here) */}
+          <div className="absolute inset-0 flex items-center justify-center text-white text-center">
+            <div>
+              <h1 className="text-4xl font-bold mb-4">Lorem Ipsum Dolor Sit</h1>
+              <p className="text-lg mb-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, eum.</p>
+              <button className="bg-blue-500 text-white px-6 py-3 rounded">Explore Now</button>
+            </div>
           </div>
         </div>
-
       </div>
-
-      <div className="relative h-full flex items-center justify-center border-2 border-black mt-8 w-full">
-        <div className="flex items-center justify-center">
-          Insert Image of doctor and square backgrounds here
-          {/* <Image
-            src={Med}
-            alt="med"
-            width={500}
-            height={500}
-            layout="responsive"
-            className="w-full absolute inset-0 object-cover z-20"
-          /> */}
-        </div>
-      </div>
-
     </section>
-  )
+  );
 }
--- app/(home-sections)/Navbar.tsx
+++ app/(home-sections)/Navbar.tsx
@@ -8,10 +8,10 @@ export default function Navbar() {
     const pathname = usePathname()
 
   return (
-    <header className='w-full'>
-        <nav className='flex flex-row justify-between items-center py-6 bg-[#5dc0b8] lg:bg-inherit px-8'>
+    <header className='w-full sticky top-0 left-0 right-0 z-50'>
+        <nav className='flex flex-row justify-between items-center py-6 lg:bg-inherit px-8'>
             <Link href={'/'}>
-                <p className='text-3xl sm:text-4xl leading-10 font-black italic text-white md:text-6xl lg:text-[#194C7A]'>LOGO</p>
+                <p className='text-3xl sm:text-4xl leading-10 font-black italic md:text-6xl text-[#194C7A]'>LOGO</p>
             </Link>
             <ul className='hidden lg:flex flex-row items-center justify-between gap-12'>
                 <li className='text-[#194C7A] xs:text-md lg:text-lg xl:text-xl font-medium leading-7'><Link href={'/'}>Home</Link></li>
@@ -24,10 +24,10 @@ export default function Navbar() {
                 <button className='hidden lg:block text-white lg:text-lg xl:text-xl font-semibold leading-7 bg-[#194C7A] hover:bg-[#47ccc7] shadow-lg hover:scale-105 duration-100 px-8 py-4 rounded-md'>Register</button>
             </div>
             <div className='block lg:hidden'>
-            <button className="flex flex-col gap-1.5 items-end">
-          <div className="w-7 rounded-md h-[2px] bg-white"></div>
-          <div className="w-7 h-[2px] rounded-md bg-white"></div>
-          <div className="w-7 h-[2px] rounded-md bg-white"></div>
+            <button className="flex flex-col gap-1 items-end">
+          <div className="w-7 rounded-md h-[2.5px] bg-[#194C7A]"></div>
+          <div className="w-7 h-[2.5px] rounded-md bg-[#194C7A]"></div>
+          <div className="w-7 h-[2.5px] rounded-md bg-[#194C7A]"></div>
         </button>
             </div>
         </nav>
