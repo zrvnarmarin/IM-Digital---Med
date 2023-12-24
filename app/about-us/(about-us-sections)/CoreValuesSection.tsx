111cfa14a72d9a994fa9ba7771b21c6fe4b378a9
76marin76 <76marin76@gmail.com>
2023-12-24 22:23:34 +0100

    core values section finished for mobile screen in about route

---
+++ app/about-us/(about-us-sections)/CoreValuesSection.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { IconsEightTestIcon } from "@/public/Icons";
+
+export default function CoreValuesSection() {
+  return (
+    <section className="w-full flex flex-col gap-8 py-16 px-8 bg-white pt-12">
+        <div className="flex flex-col items-center gap-2 relative">
+          <p className="text-9xl text-slate-200 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0">
+            01
+          </p>
+          <div className="relative z-10 text-xl uppercase font-bold">
+            Our Values
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {coreValues.map((value) => (
+            <div className="flex flex-col items-start gap-1">
+              {value.icon()}
+              <h2 className="text-center text-md md:text-lg lg:text-xl font-semibold pt-2">
+                {value.value}
+              </h2>
+              <p className="text-sm text-slate-500 pt-1">
+                {value.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+  )
+}
+
+const coreValues = [
+    {
+      icon: IconsEightTestIcon,
+      value: "Transparency",
+      description:
+        "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+    },
+    {
+      icon: IconsEightTestIcon,
+      value: "Transparency",
+      description:
+        "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+    },
+    {
+      icon: IconsEightTestIcon,
+      value: "Transparency",
+      description:
+        "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+    },
+    {
+      icon: IconsEightTestIcon,
+      value: "Transparency",
+      description:
+        "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+    },
+  ];
--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,101 +1,126 @@
-import React from 'react';
-import Image from 'next/image';
-import Navbar from '../(home-sections)/Navbar';
-import HeroSection from './(about-us-sections)/HeroSection'
-import OurFoundingStorySection from './(about-us-sections)/OurFoundingStorySection';
-import WhatSetUsApartSection from './(about-us-sections)/WhatSetUsApartSection';
-import Footer from '../components/Footer';
+import React from "react";
+import Image from "next/image";
+import Navbar from "../(home-sections)/Navbar";
+import HeroSection from "./(about-us-sections)/HeroSection";
+import CoreValuesSection from "./(about-us-sections)/CoreValuesSection";
+import { IconsEightTestIcon } from "@/public/Icons";
+import Footer from "../components/Footer";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center lg:pt-4 bg-[#FFFFFF]">
       <Navbar />
       <HeroSection />
-
-      <div className="b-2">
-        <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-star md:text-center text-[#2a2f31] leading-10 mb-8 pt-6 pb-2">
-          About Us
-        </h1>
-        <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
-          Welcome to Kompany, where our commitment to your well-being drives
-          everything we do. Founded on the principles of compassion,
-          expertise, and innovation, we strive to redefine healthcare and make
-          a positive impact on lives around the world. We believe in
-          accessible and personalized healthcare.
-        </p>
-        <br />
-        <br />
-        <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
-          Our mission is to empower individuals to take control of their
-          health and well-being through a seamless blend of cutting-edge
-          technology and compassionate care.
-        </p>
-      </div>
-
-      {/* Founding Story */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-12">
-        <div className="md:col-span-1 text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
-          <h2 className="text-xl font-bold mb-4">Our Founding Story</h2>
-          <p>
-            In the early days of 2010, two visionary minds, Dr. Allison Harper
-            and Tech Innovator Ethan Mitchell, came together with a shared
-            dream — to revolutionize healthcare and make a lasting impact on
-            the well-being of individuals worldwide.
-          </p>
-          <p>
-            Dr. Harper, a seasoned healthcare professional with a passion for
-            patient-centered care, had witnessed the gaps in accessibility and
-            personalized attention in the healthcare industry. Meanwhile,
-            Ethan Mitchell, a brilliant tech entrepreneur, envisioned a future
-            where advanced technology could seamlessly integrate with
-            healthcare, enhancing the overall experience for patients.
-          </p>
-          <p>
-            Their paths converged, and in 2011, they founded [Your Company
-            Name]. United by a commitment to innovation, compassion, and
-            excellence, Dr. Harper and Ethan Mitchell set out to create a
-            healthcare ecosystem that transcended traditional boundaries.
-          </p>
-        </div>
-        <div className="border-2 border-black h-48 md:h-full items-center justify-center">
-          Image of founders
-        </div>
-      </div>
-
-      {/* What Sets Us Apart */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 bg-[#FFFFFF] w-full py-12">
-        <div className="order-2">
-          <h2 className="text-xl font-bold mb-4">What Sets Us Apart</h2>
-          <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
-            <span className="font-semibold">Patient-Centric Approach:</span>{' '}
-            Your health is at the heart of everything we do. We prioritize
-            your needs, ensuring a personalized and empathetic experience at
-            every step.
-          </p>
-          <br />
-          <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
-            <span className="font-semibold">Innovation in Healthcare:</span>
-            We leverage the latest technologies and medical advancements to
-            provide you with the best possible care. Our commitment to
-            innovation ensures that you have access to state-of-the-art
-            solutions.
-          </p>
-          <br />
-          <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
-            <span className="font-semibold">Qualified Professionals:</span>
-            Our team comprises experienced healthcare professionals, from
-            skilled doctors to compassionate support staff. Rest assured,
-            you're in capable hands.
-          </p>
-          <br />
-        </div>
-        <div>
-          {/* Add any relevant images/icons for each point */}
-          <div className="border-2 border-black h-48 md:h-full"></div>
-        </div>
-      </div>
+      <CoreValuesSection />
 
       <Footer />
     </main>
   );
 }
+
+const coreValues = [
+  {
+    icon: IconsEightTestIcon,
+    value: "Transparency",
+    description:
+      "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+  },
+  {
+    icon: IconsEightTestIcon,
+    value: "Transparency",
+    description:
+      "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+  },
+  {
+    icon: IconsEightTestIcon,
+    value: "Transparency",
+    description:
+      "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+  },
+  {
+    icon: IconsEightTestIcon,
+    value: "Transparency",
+    description:
+      "Lorem ipsu, dolor sit amet consectur apkilitis elit. Libero, tone lota.",
+  },
+];
+
+// <div className="b-2">
+//         <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
+//           Welcome to Kompany, where our commitment to your well-being drives
+//           everything we do. Founded on the principles of compassion, expertise,
+//           and innovation, we strive to redefine healthcare and make a positive
+//           impact on lives around the world. We believe in accessible and
+//           personalized healthcare.
+//         </p>
+//         <br />
+//         <br />
+//         <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
+//           Our mission is to empower individuals to take control of their health
+//           and well-being through a seamless blend of cutting-edge technology and
+//           compassionate care.
+//         </p>
+//       </div>
+
+//       {/* Founding Story */}
+//       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-12">
+//         <div className="md:col-span-1 text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
+//           <h2 className="text-xl font-bold mb-4">Our Founding Story</h2>
+//           <p>
+//             In the early days of 2010, two visionary minds, Dr. Allison Harper
+//             and Tech Innovator Ethan Mitchell, came together with a shared dream
+//             — to revolutionize healthcare and make a lasting impact on the
+//             well-being of individuals worldwide.
+//           </p>
+//           <p>
+//             Dr. Harper, a seasoned healthcare professional with a passion for
+//             patient-centered care, had witnessed the gaps in accessibility and
+//             personalized attention in the healthcare industry. Meanwhile, Ethan
+//             Mitchell, a brilliant tech entrepreneur, envisioned a future where
+//             advanced technology could seamlessly integrate with healthcare,
+//             enhancing the overall experience for patients.
+//           </p>
+//           <p>
+//             Their paths converged, and in 2011, they founded [Your Company
+//             Name]. United by a commitment to innovation, compassion, and
+//             excellence, Dr. Harper and Ethan Mitchell set out to create a
+//             healthcare ecosystem that transcended traditional boundaries.
+//           </p>
+//         </div>
+//         <div className="border-2 border-black h-48 md:h-full items-center justify-center">
+//           Image of founders
+//         </div>
+//       </div>
+
+//       {/* What Sets Us Apart */}
+//       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 bg-[#FFFFFF] w-full py-12">
+//         <div className="order-2">
+//           <h2 className="text-xl font-bold mb-4">What Sets Us Apart</h2>
+//           <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
+//             <span className="font-semibold">Patient-Centric Approach:</span>{" "}
+//             Your health is at the heart of everything we do. We prioritize your
+//             needs, ensuring a personalized and empathetic experience at every
+//             step.
+//           </p>
+//           <br />
+//           <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
+//             <span className="font-semibold">Innovation in Healthcare:</span>
+//             We leverage the latest technologies and medical advancements to
+//             provide you with the best possible care. Our commitment to
+//             innovation ensures that you have access to state-of-the-art
+//             solutions.
+//           </p>
+//           <br />
+//           <p className="text-[#555f60] text-start md:text-start text-md sm:text-lg leading-7">
+//             <span className="font-semibold">Qualified Professionals:</span>
+//             Our team comprises experienced healthcare professionals, from
+//             skilled doctors to compassionate support staff. Rest assured, you're
+//             in capable hands.
+//           </p>
+//           <br />
+//         </div>
+//         <div>
+//           {/* Add any relevant images/icons for each point */}
+//           <div className="border-2 border-black h-48 md:h-full"></div>
+//         </div>
+//       </div>
