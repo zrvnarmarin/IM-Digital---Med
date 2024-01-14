7f922273f36b8b4ea3c45b9332f7a026bfb5d0dc
76marin76 <76marin76@gmail.com>
2024-01-14 20:34:35 +0100

    fixed layout of dynamic doctor route hero section image

--- app/our-team/[doctorName]/(doctor-sections)/HeroSection.tsx
+++ app/our-team/[doctorName]/(doctor-sections)/HeroSection.tsx
@@ -4,28 +4,22 @@ import SomeDoctorImage from "../../../../public/someDoctorImage.avif";
 
 export default function HeroSection({ doctorName, doctorSpecialisation } : { doctorName: string, doctorSpecialisation: string }) {
   return (
-    <section className="flex flex-col items-center justify-center lg:pt-4 md:px-8 lg:px-24 pb-4">
-      <section className="flex">
-        <div className="min-h-screen w-full">
-          {/* Hero Section */}
-          <div className="relative h-screen md:h-auto bg-black">
-            <Image
-              src={SomeDoctorImage}
-              alt="Hero Image"
-              className="object-cover w-full h-full md:h-auto opacity-60 bg-black"
-            />
-            {/* Hero Content (Add your text and buttons here) */}
-            <div className="absolute inset-0 flex flex-col items-center justify-end text-white text-center pt-64">
-              <p className="text-xs italic pb-4 px-4 text-start w-full">
-                {doctorSpecialisation}
-              </p>
-              <div className="bg-slate-700/90 text-3xl text-start font-bold mb-4 px-4 w-full py-3 border-slate-500 border">
-                <h1>Dr. Med. {doctorName}</h1>
-              </div>
-            </div>
-          </div>
+    <section className="relative w-full h-screen">
+      <Image
+        src={SomeDoctorImage}
+        alt="Hero Image"
+        className="object-cover w-full h-full"
+      />
+
+      <div className="absolute bottom-4 lg:bottom-4 left-0 right-0 w-full flex flex-col items-center justify-center text-white text-center">
+        <p className="text-xs italic pb-4 px-4 text-start w-full">
+        {doctorSpecialisation}
+        </p>
+        <div className="bg-slate-700/90 text-3xl text-start font-bold mb-4 px-4 w-full py-3 border-slate-500 border-y-[1px]">
+        <h1>Dr. Med. {doctorName}</h1>
         </div>
-      </section>
+      </div>
+      
     </section>
   );
-}
+};
