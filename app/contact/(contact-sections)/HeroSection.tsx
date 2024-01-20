240d6533a7651ab4bfeaff7ed9506fe7ed753ddc
zrvnarmarin <76marin76@gmail.com>
2024-01-20 02:39:42 +0100

    contact section restyled and redefined

--- app/contact/(contact-sections)/HeroSection.tsx
+++ app/contact/(contact-sections)/HeroSection.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import TeamOfDoctors from "../../../public/DoctorSmiling (1).jpg";
 
 export default function HeroSection() {
   return (
-    <div>
-      <h1 className='font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-[#2a2f31] leading-10 mb-6'>
-          Contact Us
-        </h1>
-        <p className='text-[#555f60] text-center text-md sm:text-lg leading-7 mb-8'>
-          Have questions or need assistance? Feel free to reach out to our team. We are here to help you.
-        </p>
-    </div>
-  )
+    <section className="relative w-full h-screen">
+      <div className="w-full h-full flex bg-red-400/10">
+        {/* <div className="absolute inset-0 bg-black/10"></div> */}
+        <Image
+          src={TeamOfDoctors}
+          alt="Hero Image"
+          className="object-cover w-full h-full"
+        />
+      </div>
+
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-between items-center text-white text-center">
+        {/* Element at the very top */}
+        <div className="w-full lg:w-1/2 flex flex-col text-center pt-60 px-4 gap-6">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-center text-5xl sm:text-4xl text leading-10 font-medium italic uppercase md:text-3xl text-white">
+              Sinergy
+            </h1>
+            <p className="text-center text-lg sm:text-4xl text leading-2 font-medium tracking-widest italic md:text-3xl text-white">
+              Where expertise meets the results
+            </p>
+          </div>
+          <div className="flex justify-center pt-8 p-6">
+            <Link
+              href="/contact"
+              className="bg-[#babaff]/50 text-white font-semibold px-6 py-3 uppercase"
+            >
+              Contact Us
+            </Link>
+          </div>
+        </div>
+
+        {/* Element at the very bottom */}
+        <div className="bg-[#babaff]/50 text-3xl text-center font-bold px-4 w-full py-3">
+          <h1 className="text-center text-2xl xs:text-4xl font-bold text-white p-6 uppercase">
+            Zdravlje je na prvom mjestu
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,15 @@
-import React from 'react';
-import Navbar from '../(home-sections)/Navbar';
-import Footer from '../components/Footer';
-import ContactInfoSection from './(contact-sections)/ContactInfoSection';
-import ContactFormSection from './(contact-sections)/ContactFormSection';
-import HeroSection from './(contact-sections)/HeroSection';
+import Navbar from "../(home-sections)/Navbar";
+import HeroSection from "./(contact-sections)/HeroSection";
+import Footer from "../components/Footer";
+import ContactSection from "../(home-sections)/ContactSection";
 
-export default function ContactSection() {
+export default function Home() {
   return (
-    <section className='flex flex-col items-center justify-center lg:pt-4 bg-gray-100'>
+    <main className="flex min-h-screen flex-col items-center bg-[#FFFFFF]">
       <Navbar />
-      <div className='px-8 pt-12 pb-6'>
-        <HeroSection />
-      </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-8 pb-16 px-8'>
-        <ContactFormSection />
-        <ContactInfoSection />
-      </div>
+      <HeroSection />
+      <ContactSection />
       <Footer />
-    </section>
+    </main>
   );
 }
