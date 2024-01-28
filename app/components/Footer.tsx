7183a532c6c9cdd86116948db5139c6dc4a450d0
76marin76 <76marin76@gmail.com>
2024-01-28 03:43:46 +0100

    fotter changing the layout to grid

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
           </p>
         </Link>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 pt-12">
+          <ul className="grid grid-cols-1 gap-8 pt-12">
             {footerLinks.map((footerLink) => (
               <div key={1}>
                 <h3 className="flex flex-col text-md md:text-lg gap-4 font-semibold text-white pb-6 leading-2 uppercase">
@@ -33,45 +33,46 @@ export default function Footer() {
             ))}
           </ul>
 
-          <div key={2}>
-            <h3 className="flex flex-col text-md md:text-lg gap-4 font-semibold text-white pb-6 leading-2 uppercase">
-              Follow Us
-            </h3>
-            <div className="flex flex-row gap-6">
-              {["li", "fb", "in"].map((link) => (
-                <Link
-                  href={`/${link}`}
-                  key={link}
-                  className="text-start w-fit text-md md:text-lg font-semibold text-white rounded-full px-2 py-1 "
-                >
-                  {link}
-                </Link>
-              ))}
+          <div className="pt-12 flex flex-col gap-16">
+            <div key={2}>
+              <h3 className="flex flex-col text-md md:text-lg gap-4 font-semibold text-white pb-2 leading-2 uppercase">
+                Follow Us
+              </h3>
+              <div className="flex flex-row gap-6">
+                {["li", "fb", "in"].map((link) => (
+                  <Link
+                    href={`/${link}`}
+                    key={link}
+                    className="text-start w-fit text-md md:text-lg font-semibold text-white rounded-full  py-1 "
+                  >
+                    {link}
+                  </Link>
+                ))}
+              </div>
             </div>
+            <form key={2}>
+              <h3 className=" flex flex-col text-md md:text-lg gap-2 font-semibold text-white pb-2 leading-2 uppercase">
+                Subscribe to our newsletter
+              </h3>
+              <label
+                htmlFor="phone-number"
+                className="flex flex-col text-white leading-2 gap-4 text-xs"
+              >
+                <span className="pb-2">To make your stay special and even more memorable</span>
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  id="phone-number"
+                  className="border-none text-md md:text-lg font-normal text-slate-600 px-6 py-3"
+                />
+              </label>
+              <div className="flex justify-center pt-4">
+                <button className="w-full bg-[#9595ff] text-white font-semibold px-6 py-3 uppercase">
+                  Send
+                </button>
+              </div>
+            </form>
           </div>
-
-          <form key={2}>
-            <h3 className=" flex flex-col text-md md:text-lg gap-2 font-semibold text-white pb-2 leading-2 uppercase">
-              Subscribe to our newsletter
-            </h3>
-            <label
-              htmlFor="phone-number"
-              className="flex flex-col text-white leading-2 gap-4 text-xs"
-            >
-              <span className="pb-2">To make your stay special and even more memorable</span>
-              <input
-                type="text"
-                placeholder="Enter your email"
-                id="phone-number"
-                className="border-none text-md md:text-lg font-normal text-slate-600 px-6 py-3"
-              />
-            </label>
-            <div className="flex justify-center pt-4">
-              <button className="w-full bg-[#9595ff] text-white font-semibold px-6 py-3 uppercase">
-                Send
-              </button>
-            </div>
-          </form>
 
         </div>
       </div>
