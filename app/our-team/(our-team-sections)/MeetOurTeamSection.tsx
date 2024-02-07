016357afa2a04717a54693d5f7bed9e6b914940c
76marin76 <76marin76@gmail.com>
2024-02-07 17:31:49 +0100

    /our-team route page restyled

--- app/our-team/(our-team-sections)/MeetOurTeamSection.tsx
+++ app/our-team/(our-team-sections)/MeetOurTeamSection.tsx
@@ -11,13 +11,13 @@ import { RightIcon } from "@/public/Icons";
 export default function MeetOurDoctors() {
   return (
     <section className="w-full flex flex-col gap-8 pt-10 sm:pt-12 md:pt-16 lg:pt-28 px-7 md:px-10 lg:px-24 xl:px-28 2xl:px-32 bg-white">
-      <h2 className="text-start text-2xl xs:text-4xl font-medium text-slate-600 underline-offset-8 decoration-1 underline decoration-[#babaff]">
+      <h2 className="text-start text-4xl font-bold text-slate-600">
         Meet our doctor specialists
       </h2>
       <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 pt-2">
-        <span className="text-[#babaff]">Clinical Research Training </span> and
+        <span className="text-[#80d6f7]">Clinical Research Training </span> and
           Medical Education Critical Care 
-        <span className="text-[#babaff]">
+        <span className="text-[#80d6f7]">
            {" "}Medicine Additional NIH Doctors/Researchers 
         </span>
           {" "}nical Research Training and Medical Education · Critical Care Medicine
@@ -37,7 +37,7 @@ const DoctorCard = ({ doctor }: { doctor: DoctorType }) => {
 
   return (
     <Link href={`/our-team/${lowerCaseDoctorName}`}>
-      <div className="relative group rounded overflow-hidden cursor-pointer">
+      <div className="relative group rounded overflow-hidden cursor-pointer shadow-md">
         {/* Background Image */}
         <div className="group-hover:opacity-75 transition duration-300 ease-in-out">
           <Image
@@ -49,34 +49,25 @@ const DoctorCard = ({ doctor }: { doctor: DoctorType }) => {
           />
         </div>
         {/* Text Container */}
-        <div className="absolute bottom-0 left-0 right-0 p-4 transition duration-300 ease-in-out bg-[#babaff]/80 text-white opacity-0 group-hover:opacity-100">
-          {/* Paragraph 1 */}
-          <p className="text-md md:text-lg lg:text-xl xl:text-xl font-semibold">
-            Dr. {doctor.name}
-          </p>
-          {/* Paragraph 2 */}
-          <p className="text-base md:text-md lg:text-lg mb-2">
-            {doctor.specialisation}
-          </p>
-          <div className="flex items-center pt-4 gap-2 justify-end">
-            <p className="text-md md:text-lg font-normal text-white">
+        <div className="absolute bottom-0 left-0 right-0 p-4 transition duration-300 ease-in-out bg-[#80d6f7] text-white opacity-0 group-hover:opacity-100">
+          <div className="flex items-center gap-2 justify-end">
+            <p className="text-white text-center font-medium text-lg xl:text-xl ">
               Meet Dr. {getFirstName(doctor.name)}
             </p>
             <div>
               <RightIcon
                 width="30"
                 height="30"
-                backgroundFillColor="#babaffcc"
                 iconFillColor="#ffffff"
               />
             </div>
           </div>
         </div>
       </div>
-      <p className="text-start w-full text-base xl:text-lg font-normal text-slate-500 group-hover:text-white duration-100 pt-4">
+      <p className="text-slate-500 text-start font-medium text-lg xl:text-xl w-full group-hover:text-white duration-100 pt-4">
         Dr. {doctor.name}
       </p>
-      <p className="text-start w-full text-sm xl:text-md font-light text-slate-500 group-hover:text-white duration-100 pt-2">
+      <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 group-hover:text-white duration-100 pt-2">
         {doctor.specialisation}
       </p>
     </Link>
