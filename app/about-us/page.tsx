51d9d4ad7af2e503ec7dd5cfc39a459116a4bc2a
76marin76 <76marin76@gmail.com>
2024-02-08 16:37:22 +0100

    /about-us route restyling

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
-import ElectricityIcon from "../../public/ElectricityIcon.png";
+import ElectricityIcon from "../../public/hospitalIcon.png";
 import SectionWrapper from "../components/wrappers/SectionWrapper";
 import HeroImage from "../../public/HospitalImage.jpg";
 import { RightIcon } from "@/public/Icons";
 import { coreValues } from "../data";
 import { DoctorType } from "@/app/types";
 import { TestSVG } from "../(home-sections)/WhyPeopleChooseUs";
-import { doctors, aboutUsThirdSectionArray, aboutUsFifthSectionArray } from "../data";
+import {
+  doctors,
+  aboutUsThirdSectionArray,
+  aboutUsFifthSectionArray,
+} from "../data";
 import { generateUrlFromFullName, getFirstName } from "../utils";
 import DoctorImage from "../../public/DoctorSmiling.jpg";
 import TestimonialsSection from "../(home-sections)/TestimonialsSection";
 import Footer from "../components/Footer";
+import TestImage from '../../public/someDoctorImage.avif'
 
 export default function AboutUs() {
   return (
@@ -53,18 +58,18 @@ export const HeroSection = () => {
 export const SecondSection = () => {
   return (
     <SectionWrapper>
-      <p className="text-start text-2xl xs:text-4xl font-medium text-slate-600 underline-offset-8 decoration-1 underline decoration-[#babaff]">
-        Service very description short
+      <p className="text-start text-4xl font-bold text-slate-600">
+        About us some heading
       </p>
 
       <p className="text-start w-full text-base xl:text-lg font-light text-slate-500">
-        <span className="font-bold text-[#babaff]">
+        <span className="font-bold text-[#80d6f7]">
           Pravom ste mjestu: naš specijalist
         </span>{" "}
         parodontologije detaljno će pregledati Vaše zubno meso te u skladu s
         time dati preporuku za odgovarajući tretman. Pravom ste mjestu: naš
         specijalist parodontologije detaljno će pregledati{" "}
-        <span className="font-bold text-[#babaff]">
+        <span className="font-bold text-[#80d6f7]">
           Vaše zubno meso te u skladu
         </span>{" "}
         s time dati preporuku za odgovarajući tretman. s time dati preporuku za
@@ -77,14 +82,14 @@ export const SecondSection = () => {
         {coreValues.map((coreValue) => (
           <li
             key={coreValue.id}
-            className="flex gap-4 justify-center items-center text-sm border-[1px] border-slate-200 hover:bg-[#f8f8ff] hover:text-white duration-300 rounded-md p-2"
+            className="flex gap-4 justify-center items-center text-sm border-[1px] border-slate-200 hover:bg-[#02aeef]/5 hover:text-white duration-300 rounded-md p-2"
           >
             <div className="h-full flex items-center"></div>
             <div className="flex flex-col gap-2 py-4 px-4">
               <div className="flex justify-center gap-2">
-                <TestSVG />
+                <TestSVG iconFill="#ffffff" backgroundFill="#80d6f7" />
               </div>
-              <p className="text-center w-full text-base xl:text-lg font-light text-slate-500 pt-2">
+              <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 pt-2">
                 {coreValue.description}
               </p>
             </div>
@@ -92,14 +97,14 @@ export const SecondSection = () => {
         ))}
       </ul>
 
-      <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 p-6 bg-[#babaff]/10 border border-slate-200 rounded-md">
-        <span className="text-[#babaff]">Paradontoza je podmukla bolest </span>{" "}
+      <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 p-6 bg-[#02aeef]/5 border border-slate-200 rounded-md">
+        <span className="text-[#80d6f7]">Paradontoza je podmukla bolest </span>{" "}
         desni koju je na početku lako ignorirati jer ne uzrokuje bol, ali zato
         vrlo brzo zbog izostanka odgovarajućeg parodontološkog tretmana preraste
         u Vaš problem br.1 jer dolazi do nezaustavljivog gubitka tkiva koje
         podržava zube. Parodontoza je podmukla bolest desni koju je na početku
         lako ignorirati jer ne uzrokuje bol,{" "}
-        <span className="text-[#babaff]">
+        <span className="text-[#80d6f7]">
           ali zato vrlo brzo zbog izostanka
         </span>
         odgovarajućeg parodontološkog tretmana preraste u Vaš problem br.1 jer
@@ -108,7 +113,7 @@ export const SecondSection = () => {
         uzrokuje bol, ali zato vrlo brzo zbog izostanka odgovarajućeg
         parodontološkog tretmana preraste u Vaš problem br.1 jer dolazi do
         nezaustavljivog{" "}
-        <span className="text-[#babaff]">gubitka tkiva koje podržava zube</span>
+        <span className="text-[#80d6f7]">gubitka tkiva koje podržava zube</span>
         .
       </p>
     </SectionWrapper>
@@ -118,7 +123,7 @@ export const SecondSection = () => {
 export const ThirdSection = () => {
   return (
     <SectionWrapper>
-      <p className="text-start text-2xl xs:text-4xl font-medium text-slate-600 underline-offset-8 decoration-1 underline decoration-[#babaff]">
+      <p className="text-start text-4xl font-bold text-slate-600">
         Something about clinic
       </p>
 
@@ -133,7 +138,7 @@ export const ThirdSection = () => {
               className="object-cover w-full h-full md:h-auto opacity-60 bg-black rounded-md"
             />
           </div>
-          <div className="flex flex-col items-center justify-center gap-6 p-8 bg-[#babaff]/10 rounded-md">
+          <div className="flex flex-col items-center justify-center gap-6 p-8 bg-[#02aeef]/5 rounded-md">
             <p className="w-full text-slate-500 text-start font-normal text-xl xl:text-2xl">
               Service short description{" "}
             </p>
@@ -145,14 +150,9 @@ export const ThirdSection = () => {
                 >
                   <div className="flex flex-row items-center gap-2">
                     <div className="flex items-center  gap-2 rounded-full border-slate-800 p-2">
-                      <div className="bg-[#babaff] rounded-full p-2 border border-[#8484f2] ">
-                        <Image
-                          src={ElectricityIcon}
-                          alt="icon"
-                          width={20}
-                          height={20}
-                        />
-                      </div>
+                    <div className="bg-[#02aeef]/50 rounded-full p-2 ">
+            <Image src={ElectricityIcon} alt="icon" width={15} height={15} />
+          </div>
                     </div>
                     <p className="text-start w-full text-base xl:text-lg font-light text-slate-500">
                       {listItem.text}
@@ -176,12 +176,12 @@ export const ThirdSection = () => {
               className="object-cover w-full h-full md:h-auto opacity-60 bg-black rounded-md"
             />
           </div>
-          <div className="flex flex-col items-center justify-center order-1 gap-8 p-8 bg-[#babaff]/10 rounded-md">
+          <div className="flex flex-col items-center justify-center order-1 gap-8 p-8 bg-[#02aeef]/5 rounded-md">
             <p className="w-full text-slate-500 text-start font-normal text-xl xl:text-2xl">
               Service short description{" "}
             </p>
             <p className="text-start w-full text-base xl:text-lg font-light text-slate-500">
-              <span className="text-[#babaff]">
+              <span className="text-[#80d6f7]">
                 Lorem ipsum, dolor sit amet consectetur
               </span>{" "}
               adipisicing elit. Cum itaque repellat veritatis explicabo odio
@@ -190,7 +190,7 @@ export const ThirdSection = () => {
               asperiores? Commodi. Sunt impedit temporibus rem. Suscipit
               temporibus impedit molestias! Laboriosam alias enim vitae,
               excepturi reprehenderit deleniti animi neque{" "}
-              <span className="text-[#babaff]">
+              <span className="text-[#80d6f7]">
                 labore quod maiores aperiam velit voluptas
               </span>
             </p>
@@ -204,31 +204,31 @@ export const ThirdSection = () => {
 export const FourthSection = () => {
   return (
     <SectionWrapper>
-      <p className="text-start text-2xl xs:text-4xl font-medium text-slate-600 underline-offset-8 decoration-1 underline decoration-[#babaff]">
+      <p className="text-start text-4xl font-bold text-slate-600">
         Something about clinic
       </p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="p-6 bg-[#babaff]/10 rounded-md order-2">
+        <div className="p-6 bg-[#02aeef]/5 rounded-md order-2">
           <p className="w-full text-slate-500 text-start font-normal text-xl xl:text-2xl">
             Neki naslov
           </p>
           <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 pt-2">
-            <span className="text-[#babaff]">
+            <span className="text-[#80d6f7]">
               Paradontoza je podmukla bolest{" "}
             </span>{" "}
             desni koju je na početku lako ignorirati jer ne uzrokuje bol, ali
             preraste u Vaš problem br.1 jer dolazi do nezaustavljivog gubitka
             tkiva koje podržava zube. Parodontoza je podmukla bolest desni koju
             je na početku lako ignorirati jer ne uzrokuje bol,{" "}
-            <span className="text-[#babaff]">
+            <span className="text-[#80d6f7]">
               ali zato vrlo brzo zbog izostanka
             </span>{" "}
             <br /> <br />
             odgovarajućeg parodontološkog tretmana preraste u Vaš problem br.1
             odgovarajućeg parodontološkog tretmana preraste u Vaš problem br.1
             jer dolazi do nezaustavljivog{" "}
-            <span className="text-[#babaff]">
+            <span className="text-[#80d6f7]">
               gubitka tkiva koje podržava zube
             </span>
           </p>
@@ -251,40 +251,37 @@ export const FourthSection = () => {
 export const FifthSection = () => {
   return (
     <SectionWrapper>
-      <p className="text-start text-2xl xs:text-4xl font-medium text-slate-600 underline-offset-8 decoration-1 underline decoration-[#babaff]">
-        Something about clinic
+      <p className="text-start text-4xl font-bold text-slate-600">
+        Fifth section
       </p>
-      <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {aboutUsFifthSectionArray.map((item) => (
-          <li
-            key={item.id}
-            className="flex gap-4 items-center text-sm border-[1px] border-slate-200 hover:bg-[#f8f8ff] hover:text-white duration-300 rounded-md p-2"
-          >
-            <div className="h-full flex items-center"></div>
-            <div className="flex flex-col gap-2 py-4 px-4">
-              <div className="flex justify-start gap-2">
-                <TestSVG />
-              </div>
-              <p className="w-full text-slate-500 text-start font-normal text-xl xl:text-2xl">
-                {item.title}
-              </p>
-              <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 pt-2">
-                {item.text}
-              </p>
-            </div>
-          </li>
+          <div
+          key={item.id}
+          className={`group flex flex-col rounded-md items-center p-4 md:p-6 lg:p-8 border drop-shadow-2xl border-slate-200 hover:bg-[#02aeef]/5 hover:text-white duration-300`}
+        >
+          <div>
+            <TestSVG iconFill="#ffffff" backgroundFill="#80d6f7" />
+          </div>
+          <h2 className="text-slate-500 text-center font-medium px-6 py-3 text-lg xl:text-xl pt-2 lg:pt-4">
+            {item.title}
+          </h2>
+          <p className="text-center w-full text-base xl:text-lg font-light text-slate-500 pt-2 lg:pt-4">
+            {item.text}
+          </p>
+        </div>
         ))}
       </ul>
 
       <div className="flex flex-col gap-4">
         <p className="text-start w-full text-base xl:text-lg font-light text-slate-500">
-          <span className="font-bold text-[#babaff]">
+          <span className="font-bold text-[#80d6f7]">
             Pravom ste mjestu: naš specijalist
           </span>{" "}
           parodontologije detaljno će pregledati Vaše zubno meso te u skladu s
           time dati preporuku za odgovarajući tretman. Pravom ste mjestu: naš
           specijalist parodontologije detaljno će pregledati{" "}
-          <span className="font-bold text-[#babaff]">
+          <span className="font-bold text-[#80d6f7]">
             Vaše zubno meso te u skladu
           </span>{" "}
           s time dati preporuku za odgovarajući tretman. s time dati preporuku
@@ -294,13 +291,13 @@ export const FifthSection = () => {
         </p>
 
         <p className="text-start w-full text-base xl:text-lg font-light text-slate-500">
-          <span className="font-bold text-[#babaff]">
+          <span className="font-bold text-[#80d6f7]">
             Pravom ste mjestu: naš specijalist
           </span>{" "}
           parodontologije detaljno će pregledati Vaše zubno meso te u skladu s
           time dati preporuku za odgovarajući tretman. Pravom ste mjestu: naš
           specijalist parodontologije detaljno će pregledati{" "}
-          <span className="font-bold text-[#babaff]">
+          <span className="font-bold text-[#80d6f7]">
             Vaše zubno meso te u skladu
           </span>{" "}
           s time dati preporuku za odgovarajući tretman. s time dati preporuku
@@ -309,8 +306,8 @@ export const FifthSection = () => {
           preporuku za odgovarajući tretman.
         </p>
 
-        <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 p-6 bg-[#babaff]/10 border border-slate-200 rounded-md">
-          <span className="text-[#babaff]">
+        <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 p-6 bg-[#02aeef]/5 border border-slate-200 rounded-md">
+          <span className="text-[#80d6f7]">
             Paradontoza je podmukla bolest{" "}
           </span>{" "}
           desni koju je na početku lako ignorirati jer ne uzrokuje bol, ali zato
@@ -318,7 +315,7 @@ export const FifthSection = () => {
           preraste u Vaš problem br.1 jer dolazi do nezaustavljivog gubitka
           tkiva koje podržava zube. Parodontoza je podmukla bolest desni koju je
           na početku lako ignorirati jer ne uzrokuje bol,{" "}
-          <span className="text-[#babaff]">
+          <span className="text-[#80d6f7]">
             ali zato vrlo brzo zbog izostanka
           </span>
           odgovarajućeg parodontološkog tretmana preraste u Vaš problem br.1 jer
@@ -327,7 +324,7 @@ export const FifthSection = () => {
           ignorirati jer ne uzrokuje bol, ali zato vrlo brzo zbog izostanka
           odgovarajućeg parodontološkog tretmana preraste u Vaš problem br.1 jer
           dolazi do nezaustavljivog{" "}
-          <span className="text-[#babaff]">
+          <span className="text-[#80d6f7]">
             gubitka tkiva koje podržava zube
           </span>
           .
@@ -335,7 +332,7 @@ export const FifthSection = () => {
         <div className="flex justify-center pt-8 px-6">
           <Link
             href="/aboutus"
-            className="bg-[#babaff]/50 text-white font-semibold px-6 py-3 uppercase"
+            className="bg-[#02aeef]/50 hover:bg-[#02aeef]/70 duration-100 rounded-md shadow-2xl text-white font-medium px-6 py-3"
           >
             Link to somewhere
           </Link>
@@ -349,7 +346,7 @@ export const SixthSection = () => {
   return (
     <SectionWrapper>
       <div className="w-fit bg-white flex flex-row items-center justify-start gap-2 pt-2">
-        <h2 className="text-center text-2xl xs:text-4xl font-medium text-slate-600 underline-offset-8 decoration-1 underline decoration-[#babaff]">
+        <h2 className="text-start text-4xl font-bold text-slate-600">
           Meet Our Team
         </h2>
       </div>
@@ -361,7 +358,7 @@ export const SixthSection = () => {
       <div className="flex justify-center pt-8 px-6">
         <Link
           href="/our-team"
-          className="bg-[#babaff]/50 text-white font-semibold px-6 py-3 uppercase"
+          className="bg-[#02aeef]/50 hover:bg-[#02aeef]/70 duration-100 rounded-md shadow-2xl text-white font-medium px-6 py-3"
         >
           Meet The Rest
         </Link>
@@ -375,46 +372,37 @@ const DoctorCard = ({ doctor }: { doctor: DoctorType }) => {
 
   return (
     <Link href={`/our-team/${lowerCaseDoctorName}`}>
-      <div className="relative group rounded overflow-hidden cursor-pointer">
+      <div className="relative group rounded overflow-hidden cursor-pointer shadow-md">
         {/* Background Image */}
         <div className="group-hover:opacity-75 transition duration-300 ease-in-out">
           <Image
             className="object-cover w-full h-54 md:h-72 lg:h-80 xl:h-96 transform group-hover:scale-105 duration-300"
-            src={DoctorImage}
+            src={TestImage}
             alt="Card Background"
             width={0}
             height={0}
           />
         </div>
         {/* Text Container */}
-        <div className="absolute bottom-0 left-0 right-0 p-4 transition duration-300 ease-in-out bg-[#babaff]/80 text-white opacity-0 group-hover:opacity-100">
-          {/* Paragraph 1 */}
-          <p className="text-md md:text-lg lg:text-xl xl:text-xl font-semibold">
-            Dr. {doctor.name}
-          </p>
-          {/* Paragraph 2 */}
-          <p className="text-base md:text-md lg:text-lg mb-2">
-            {doctor.specialisation}
-          </p>
-          <div className="flex items-center pt-4 gap-2 justify-end">
-            <p className="text-md md:text-lg font-normal text-white">
+        <div className="absolute bottom-0 left-0 right-0 p-4 transition duration-300 ease-in-out bg-[#80d6f7] text-white opacity-0 group-hover:opacity-100">
+          <div className="flex items-center gap-2 justify-end">
+            <p className="text-white text-center font-medium text-lg xl:text-xl ">
               Meet Dr. {getFirstName(doctor.name)}
             </p>
             <div>
               <RightIcon
                 width="30"
                 height="30"
-                backgroundFillColor="#babaffcc"
                 iconFillColor="#ffffff"
               />
             </div>
           </div>
         </div>
       </div>
-      <p className="text-start w-full text-base xl:text-lg font-normal text-slate-500 group-hover:text-white duration-100 pt-4">
+      <p className="text-slate-500 text-start font-medium text-lg xl:text-xl w-full group-hover:text-white duration-100 pt-4">
         Dr. {doctor.name}
       </p>
-      <p className="text-start w-full text-sm xl:text-md font-light text-slate-500 group-hover:text-white duration-100 pt-2">
+      <p className="text-start w-full text-base xl:text-lg font-light text-slate-500 group-hover:text-white duration-100 pt-2">
         {doctor.specialisation}
       </p>
     </Link>
@@ -435,7 +423,7 @@ const SeventhSection = () => {
       <div className="flex justify-center text-center pt-8 px-6">
         <Link
           href="/contact-us"
-          className="bg-[#babaff]/50 text-white font-semibold px-6 py-3 uppercase"
+          className="bg-[#02aeef]/50 hover:bg-[#02aeef]/70 duration-100 rounded-md shadow-2xl text-white font-medium px-6 py-3"
         >
           Contact us
         </Link>
